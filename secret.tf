bd208f6cba7dbd1dd31b8d80d74081b50a8761e8
gulnaza <yantsiyeva@gmail.com>
2023-04-06 22:59:27 -0400

    secret tag added

--- secret.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_secretsmanager_secret" "extend_secret" {
-  name = "extend-interview/gulnaza"
-}
-
-resource "aws_secretsmanager_secret_version" "extend_secret" {
-  secret_id     = aws_secretsmanager_secret.extend_secret.id
-  secret_string = formatdate("MM/DD/YYYY", timestamp())
-}
-
